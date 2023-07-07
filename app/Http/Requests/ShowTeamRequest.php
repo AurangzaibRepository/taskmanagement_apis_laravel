dbc65dec4c01bad077827d379d95b1530dcc1d41
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-07 11:56:00 +0500

    Creates reqeust for show team action

---
+++ app/Http/Requests/ShowTeamRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShowTeamRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
