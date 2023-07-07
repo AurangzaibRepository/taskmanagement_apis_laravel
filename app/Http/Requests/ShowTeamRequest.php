f9c0d21ca697860189c777ecd460191d58f20027
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-07 12:35:27 +0500

    Defines messages function

--- app/Http/Requests/ShowTeamRequest.php
+++ app/Http/Requests/ShowTeamRequest.php
@@ -12,4 +12,12 @@ class ShowTeamRequest extends FormRequest
             'id' => 'integer|exists:teams',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'integer' => ':attribute must be an integer',
+            'id.exists' => 'Team not found',
+        ];
+    }
 }
