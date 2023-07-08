0666fafa1b0beda1b69b3ee3774b7970e67b8f4d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-08 19:57:25 +0500

    Defines rules

--- app/Http/Requests/ProjectListingRequest.php
+++ app/Http/Requests/ProjectListingRequest.php
@@ -9,6 +9,8 @@ class ProjectListingRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'pageNumber' => 'integer',
+            'team_id' => 'nullable|integer|exists:teams,id',
         ];
     }
 }
