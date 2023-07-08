a4b2e8628cd0b3207b7bafe3fcdbec16dd562459
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-08 16:26:41 +0500

    Defines mesasges

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -16,4 +16,16 @@ class UpdateProjectRequest extends FormRequest
             'team_id' => 'required|integer|exists:teams,id',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'integer' => ':attribute must be an integer',
+            'required' => ':attribute is required',
+            'code.max' => 'code must not be greater than 10 characters',
+            'code.unique' => 'code already exists',
+            'name.max' = 'name must not be greater than 100 characters',
+            'team_id.exists' => 'Team not found',
+        ];
+    }
 }
