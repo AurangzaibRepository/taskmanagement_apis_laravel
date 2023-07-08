8751a624777978729992f4715ba19f9c6e649194
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-08 16:09:03 +0500

    Defines rules

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -9,6 +9,11 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'id' => 'integer|exists:projects',
+            'code' => 'required|max:10,unique:projects,code,'.$this->route('code'),
+            'name' => 'required|max:100',
+            'description' => 'required',
+            'team_id' => 'required|integer|exists:teams,id',
         ];
     }
 }
