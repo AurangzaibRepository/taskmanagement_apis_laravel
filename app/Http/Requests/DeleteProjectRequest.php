e0acaaaf0a80702c0abb0f81d2ac85885a6b487a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-09 13:24:08 +0500

    Defines prepare for validation function

--- app/Http/Requests/DeleteProjectRequest.php
+++ app/Http/Requests/DeleteProjectRequest.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DeleteProjectRequest extends FormRequest
 {
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'id' => $this->route('project'),
+        ]);
+    }
+
     public function rules(): array
     {
         return [
