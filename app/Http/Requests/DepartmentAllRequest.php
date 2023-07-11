698be9e4b06d562497411c20116c63f42d12ee57
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 17:07:21 +0500

    Creates Department all request

---
+++ app/Http/Requests/DepartmentAllRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DepartmentAllRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
