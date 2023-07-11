8821cdbe3631c5af49cdcd7490c7d46514f3569a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 11:46:19 +0500

    Creates listener for project deleted event

---
+++ app/Listeners/DeleteProjectTasks.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ProjectDeleted;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class DeleteProjectTasks
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ProjectDeleted $event): void
+    {
+        //
+    }
+}
