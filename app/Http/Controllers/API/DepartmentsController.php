b26e44e43c4748077a252a34de1f36fd31677deb
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 17:02:01 +0500

    Implements all function

--- app/Http/Controllers/API/DepartmentsController.php
+++ app/Http/Controllers/API/DepartmentsController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Department;
 
 class DepartmentsController extends Controller
 {
     public function __construct(
         private Department $department,
-    ) { 
+    ) {
+    }
+
+    public function all(int $teamId): JsonResponse
+    {
+        $data = $this->department->getAll($teamId);
+
+        return getResponse(true, $data);
     }
 }
