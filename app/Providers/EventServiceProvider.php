811ba70222d116458b99bdfeadf153cf5eeab756
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 14:59:47 +0500

    Registers team deleted event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\ProjectDeleted;
+use App\Events\TeamDeleted;
 use App\Listeners\DeleteProjectTasks;
+use App\Listeners\DeleteTeamProjects;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -22,6 +24,9 @@ class EventServiceProvider extends ServiceProvider
         ],
         ProjectDeleted::class => [
             DeleteProjectTasks::class,
+        ],
+        TeamDeleted::class => [
+            DeleteTeamProjects::class,
         ],
     ];
 
