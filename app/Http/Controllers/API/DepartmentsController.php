e6f46ac3b6e8476f0ed34121589bddd31d880dda
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 16:54:13 +0500

    Includes model

--- app/Http/Controllers/API/DepartmentsController.php
+++ app/Http/Controllers/API/DepartmentsController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Department;
 
 class DepartmentsController extends Controller
 {
+    public function __construct(
+        private Department $department,
+    ) { 
+    }
 }
