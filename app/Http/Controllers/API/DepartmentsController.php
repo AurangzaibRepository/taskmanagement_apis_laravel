a4615ba0040b87d5766b77946080d38ba6810327
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 17:03:47 +0500

    Includes department all request

--- app/Http/Controllers/API/DepartmentsController.php
+++ app/Http/Controllers/API/DepartmentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DepartmentAllRequest;
 use App\Models\Department;
 
 class DepartmentsController extends Controller
@@ -12,7 +13,7 @@ class DepartmentsController extends Controller
     ) {
     }
 
-    public function all(int $teamId): JsonResponse
+    public function all(DepartmentAllRequest $request, int $teamId): JsonResponse
     {
         $data = $this->department->getAll($teamId);
 
