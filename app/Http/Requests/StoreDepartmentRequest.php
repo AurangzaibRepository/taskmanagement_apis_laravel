2346b4ada18ba7707e1650f3d33ac4023013bd6e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 17:39:44 +0500

    Defines rules

--- app/Http/Requests/StoreDepartmentRequest.php
+++ app/Http/Requests/StoreDepartmentRequest.php
@@ -9,6 +9,9 @@ class StoreDepartmentRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'name' => 'required|max:100',
+            'description' => 'required',
+            'team_id' => 'required|integer|exists:teams,id',
         ];
     }
 }
