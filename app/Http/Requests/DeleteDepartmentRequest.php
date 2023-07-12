5a9dec63f0205b30db6043825c1f5a160866cb05
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-12 15:43:55 +0500

    Implements failed validation function

--- app/Http/Requests/DeleteDepartmentRequest.php
+++ app/Http/Requests/DeleteDepartmentRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpJsonException;
 use Illuminate\Http\JsonResponse;
 
@@ -29,5 +29,13 @@ class DeleteDepartmentRequest extends FormRequest
             'integer' => ':attribute must be an integer',
             'id.exists' => 'Department not found',
         ];
+    }
+
+    protected function failedValidation(Validator $validator): JsonResponse
+    {
+        throw new HttpJsonException(getResponse(
+            false,
+            $validator->messages()->all(),
+        ));
     }
 }
