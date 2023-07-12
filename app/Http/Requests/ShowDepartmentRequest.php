f5e710dab0ec1bad5ba054bd98a8838963a968c2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-12 12:55:00 +0500

    Implement prpare for validation function

--- app/Http/Requests/ShowDepartmentRequest.php
+++ app/Http/Requests/ShowDepartmentRequest.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ShowDepartmentRequest extends FormRequest
 {
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'id' => $this->route('department'),
+        ]);
+    }
+
     public function rules(): array
     {
         return [
