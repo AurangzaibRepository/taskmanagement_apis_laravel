3d66f6faca5c76c9747cc5c08ad60ebee7954ce5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-12 16:31:10 +0500

    Incldues namespaces

--- app/Http/Requests/DepartmentListingRequest.php
+++ app/Http/Requests/DepartmentListingRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminat\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class DepartmentListingRequest extends FormRequest
 {
