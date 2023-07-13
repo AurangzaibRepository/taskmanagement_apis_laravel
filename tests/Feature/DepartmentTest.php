d95a5cd516a703ef51a8ae450992ea3cdac63f94
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-13 17:07:32 +0500

    Implements test for all

--- tests/Feature/DepartmentTest.php
+++ tests/Feature/DepartmentTest.php
@@ -2,8 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\Team;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class DepartmentTest extends TestCase
 {
+    public function test_department_all(): void
+    {
+        $teamId = Team::first()->id;
+        $response = $this->getJson("/api/departments/all/{$teamId}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('data')
+            );
+    }
 }
