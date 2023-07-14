6adfac63eb5e093e0903a793aeeaa6ca604979a1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-14 16:42:54 +0500

    Implements test method for all

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,8 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\Team;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class ProjectTest extends TestCase
 {
+    public function test_project_all(): void
+    {
+        $teamId = Team::first()->id;
+        $response = $this->getJson("/api/projects/all/{$teamId}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('data')
+            );
+    }
 }
