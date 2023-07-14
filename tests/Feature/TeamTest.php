d8e0cdd82653231a52c327e097c74905da4d5c95
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-14 15:00:07 +0500

    Implements test for details

--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Team;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -23,6 +24,18 @@ class TeamTest extends TestCase
         $response = $this->postJson('/api/teams/listing', [
             'page_number' => 1,
         ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('data')
+            );
+    }
+
+    public function test_team_details(): void
+    {
+        $teamId = Team::first()->id;
+        $response = $this->getJson("/api/teams/{$teamId}");
 
         $response
             ->assertStatus(200)
