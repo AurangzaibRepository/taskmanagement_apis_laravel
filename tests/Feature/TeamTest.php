892d9b7ad6b712fc04440d47c53771e5a86c6020
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-14 14:37:39 +0500

    Implements test for team all

--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -2,8 +2,19 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class TeamTest extends TestCase
 {
+    public function test_team_all(): void
+    {
+        $response = $this->getJson('/api/teams/all');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('data')
+            );
+    }
 }
