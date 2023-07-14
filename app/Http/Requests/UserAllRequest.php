27d4d09a2219c7e020720be3a25e0c9369ad871e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-14 19:45:55 +0500

    Includes namespaces

--- app/Http/Requests/UserAllRequest.php
+++ app/Http/Requests/UserAllRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\JsonResponse;
 
 class UserAllRequest extends FormRequest
 {
