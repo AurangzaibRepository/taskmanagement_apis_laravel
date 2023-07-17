3d4c13da4bf7a2e506aace362a73f360980df6fa
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 18:21:51 +0500

    Implements failed vadiation function

--- app/Http/Requests/ShowUserRequest.php
+++ app/Http/Requests/ShowUserRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 
@@ -29,5 +29,13 @@ class ShowUserRequest extends FormRequest
             'integer' => ':attribute must be an inetger',
             'id.exists' => 'User not found',
         ];
+    }
+
+    protected function failedValidation(Validator $validator): JsonResponse
+    {
+        throw new httpResponseException(getResponse(
+            false,
+            $validator->messages()->all(),
+        ));
     }
 }
