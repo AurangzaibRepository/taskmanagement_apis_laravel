10f11f6c4b045b5ccdd495ce1cdbf47d53bd8bec
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 12:55:05 +0500

    Defines rules

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\UserRoleEnum;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
 
 class StoreUserRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
+            'first_name' => 'required|max:50',
+            'last_name' => 'required|max:50',
+            'email' => 'required|email',
+            'phone_number' => 'required',
+            'password' => 'required',
+            'team_id' => 'required|integer|exists:team,id',
+            'department_id' => 'required|integer|exists:departments,id',
+            'picture' => 'nullable|image',
+            'role' => [new Enum(UserRoleEnum::class)],
         ];
     }
 }
