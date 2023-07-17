d1818589c5826e20595d212c9c2bfd8c16068cfe
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 19:26:51 +0500

    Implements messages function

--- app/Http/Requests/UserListingRequest.php
+++ app/Http/Requests/UserListingRequest.php
@@ -9,10 +9,21 @@ class UserListingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'page_number' => 'integer',
+            'page_number' => 'required|integer',
             'email' => 'nullable|email',
             'team_id' => 'nullable|integer|exists:teams,id',
             'department_id' => 'nullable|integer|exists:departments,id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => ':attribute is required',
+            'integer' => ':attribute must be an integer',
+            'email.email' => 'Invalid email',
+            'team_id.exists' => 'Team not found',
+            'department_id.exists' => 'Department not found',
         ];
     }
 }
