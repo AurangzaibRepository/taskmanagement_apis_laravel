94c45f54bf9b01f7b6b8fa3bcd4a1f882828cc17
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-17 16:06:36 +0500

    Defines rules

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -9,6 +9,16 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'id' => 'integer|exists:users',
+            'first_name' => 'required|max:50',
+            'last_name' => 'required|max:50',
+            'email' => 'required|email|unique:users|ends_with:gmail.com,laravel.com',
+            'phone_number' => 'required',
+            'password' => 'required',
+            'team_id' => 'required|integer|exists:teams,id',
+            'department_id' => 'required|integer|exists:departments,id',
+            'image' => 'nullable|image',
+            'role' => 'required|in:Admin,User',
         ];
     }
 }
