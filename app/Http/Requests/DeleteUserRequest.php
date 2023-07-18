5c7e24c1f692c6a53fc2e66b70fa0a23b439f584
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-18 15:05:19 +0500

    Implements prepare for valdiation function

--- app/Http/Requests/DeleteUserRequest.php
+++ app/Http/Requests/DeleteUserRequest.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DeleteUserRequest extends FormRequest
 {
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'id' => $this->route('user'),
+        ]);
+    }
+
     public function rules(): array
     {
         return [
