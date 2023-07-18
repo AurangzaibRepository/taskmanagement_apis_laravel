13878d150ce19134bf42e3b1297f768123562a2b
Auranzgaib Najam <aurangzaib_najam@hotmail.com>
2023-07-18 15:58:26 +0500

    Implements test for all method

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,8 +2,23 @@
 
 namespace Tests\Feature;
 
+use App\Models\Department;
+use App\Models\Team;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    public function test_user_all(): void
+    {
+        $teamId = Team::first()->id;
+        $departmentId = Department::first()->id;
+        $response = $this->getJson("/api/users/all/{$teamId}/{$departmentId}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('data')
+            );
+    }
 }
