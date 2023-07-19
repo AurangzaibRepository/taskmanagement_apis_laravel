c418b5e68196043655d0f129d20f3b086d8e93dc
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-19 11:54:53 +0500

    Declares function for all

--- app/Http/Controllers/API/TasksController.php
+++ app/Http/Controllers/API/TasksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,9 @@ class TasksController extends Controller
     public function __construct(
         private Task $task,
     ) {
+    }
+
+    public function all(int $projectId): JsonResponse
+    {
     }
 }
