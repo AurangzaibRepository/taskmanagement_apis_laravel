c9aa743d6e9b79925a6ac9f88f5077f4e94269e1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-19 17:55:40 +0500

    Creates task all request

---
+++ app/Http/Requests/TaskAllRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskAllRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
