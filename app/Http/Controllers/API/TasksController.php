dec8393711ad2b6e832588ab868dd5c3dbceca7b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-19 11:50:25 +0500

    Includes task model

--- app/Http/Controllers/API/TasksController.php
+++ app/Http/Controllers/API/TasksController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
+    public function __construct(
+        private Task $task,
+    ) {
+    }
 }
