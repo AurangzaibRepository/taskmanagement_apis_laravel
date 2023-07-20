6d39d9d7aab1f731ed0156198a12341f1c9d7863
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-20 19:11:50 +0500

    Defines rule

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -9,6 +9,12 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'id' => 'integer|exists:tasks',
+            'title' => 'required|max:80',
+            'description' => 'required',
+            'project_id' => 'required|integer|exists:projects,id',
+            'category_id' => 'required|integer|exists:categories,id',
+            'user_id' => 'required|integer|exists:users,id',
         ];
     }
 }
