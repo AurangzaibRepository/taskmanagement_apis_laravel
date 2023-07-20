f644e36848bea408caae9dbe77684b3ce8107c23
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-20 11:35:38 +0500

    Defines enum values for task status

--- app/Enums/TaskStatusEnum.php
+++ app/Enums/TaskStatusEnum.php
@@ -1,3 +1,12 @@
 <?php
 
 namespace App\Enums;
+
+enum TaskStatusEnum: string
+{
+    case Open = 'Open';
+    case Inprogress = 'In progress';
+    case Done = 'Done';
+    case Closed = 'Closed';
+    case Cancelled = 'Cancelled';
+}
