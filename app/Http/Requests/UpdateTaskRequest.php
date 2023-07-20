815529579fb42705dd56c28147c77fa3fc2866db
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-20 19:09:41 +0500

    Creates update request

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTaskRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
