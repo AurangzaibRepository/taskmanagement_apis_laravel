cbe6b908f0cc6bdcc285b59cc66d9ee58199b058
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-20 20:30:35 +0500

    Includes destroy request

--- app/Http/Controllers/API/TasksController.php
+++ app/Http/Controllers/API/TasksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DestroyTaskRequest;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\TaskAllRequest;
 use App\Http\Requests\ShowTaskRequest;
@@ -45,7 +46,7 @@ class TasksController extends Controller
         return getResponse(true, null, 'Task updated successfully');
     }
 
-    public function destroy(int $id): JsonReponse
+    public function destroy(DestroyTaskRequest $request, int $id): JsonReponse
     {
     }
 }
