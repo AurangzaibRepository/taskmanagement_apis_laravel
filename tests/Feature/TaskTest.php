32c5fa9a6152e86aa0dd0dd79858ad99f36ac5b5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-21 16:32:29 +0500

    Implements test method fo rlisting

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -17,6 +19,25 @@ class TaskTest extends TestCase
             ->assertStatus(200)
             ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
                 ->has('data')
+            );
+    }
+
+    public function test_task_listing(): void
+    {
+        $payload = [
+            'page_number' => 1,
+            'project_id' => Project::first()->id,
+            'category_id' => Category::first()->id,
+            'user_id' => User::first()->id,
+            'status' => 'Open',
+        ];
+
+        $response = $this->postJson('/api/tasks/listing', $payload);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->hasAll(['data.page_number', 'data.records_count', 'data.records'])
             );
     }
 }
