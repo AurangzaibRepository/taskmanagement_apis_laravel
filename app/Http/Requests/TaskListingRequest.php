3be167c3d0a993aff6ddc316d1e75bf46a827536
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-21 13:20:36 +0500

    Defines rule

--- app/Http/Requests/TaskListingRequest.php
+++ app/Http/Requests/TaskListingRequest.php
@@ -9,6 +9,9 @@ class TaskListingRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'project_id' => 'integer|exists:projects,id',
+            'category_id' => 'integer|exists:categories,id',
+            'user_id' => 'integer|exists:users,id',
         ];
     }
 }
