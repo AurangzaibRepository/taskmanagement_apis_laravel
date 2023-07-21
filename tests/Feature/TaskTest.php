72b334494e1e305cd0e10d455dbd12a2f3c993f7
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-21 15:07:25 +0500

    Implements test  method for all

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,10 +2,21 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Project;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
+    public function test_task_all(): void
+    {
+        $projectId = Project::first()->id;
+        $response = $this->getJson("/api/tasks/all/{$projectId}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('data')
+            );
+    }
 }
