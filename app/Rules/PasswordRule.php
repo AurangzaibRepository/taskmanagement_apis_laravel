469038117b2840992b121d20a87720ff8415a325
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 17:06:47 +0500

    Injects email va constructor

--- app/Rules/PasswordRule.php
+++ app/Rules/PasswordRule.php
@@ -9,9 +9,14 @@ use Illuminate\Support\Facades\Hash;
 
 class PasswordRule implements ValidationRule
 {
+    public function __construct($email)
+    {
+        $this->requestEmail = $email;
+    }
+
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $user = User::where('email', $attributes['email'])
+        $user = User::where('email', $this->reqeustEmail)
             ->first();
 
         if (! Hash::check($attribute['password'], $user->password)) {
