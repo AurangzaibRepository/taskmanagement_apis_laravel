037b17e5d4c50aa7587eca338fe94e1dbec316f8
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 15:13:32 +0500

    Declares login action

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,5 +12,9 @@ class AuthController extends Controller
     public function __construct(
         private User $user,
     ) {
+    }
+
+    public function login(): JsonResponse
+    {
     }
 }
