c8e02c34bc8f6bd71144c9c36ad393da6f78f127
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 16:52:22 +0500

    Implements logic to check password

--- app/Rules/PasswordRule.php
+++ app/Rules/PasswordRule.php
@@ -2,12 +2,20 @@
 
 namespace App\Rules;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Hash;
 
 class PasswordRule implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+        $user = User::where('email', $attributes['email'])
+            ->first();
+
+        if (! Hash::check($attribute['password'], $user->password)) {
+            $fail('Invalid credentials');
+        }
     }
 }
