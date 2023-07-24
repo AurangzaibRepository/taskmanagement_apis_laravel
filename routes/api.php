6fca0a1d0e39c47414a365b65b20586183f0606b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 15:09:37 +0500

    Applies eslinting

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('auth')->group(function() {
+Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 });
 
