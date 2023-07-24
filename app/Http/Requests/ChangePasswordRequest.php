38f0db802574221f653cd6d1d11e97a4810d7b0a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 17:51:18 +0500

    Defines messages function

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -15,4 +15,13 @@ class ChangePasswordRequest extends FormRequest
             'new_password' => 'required',
         ];
     }
+
+    public function messages(): void
+    {
+        return [
+            'required' => ':attribute is required',
+            'email.email' => 'Invalid email',
+            'email.exists' => 'Email not found',
+        ];
+    }
 }
