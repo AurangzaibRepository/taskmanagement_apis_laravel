7ec54110fee1ee8db6e54d53515cc176152f1558
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 16:18:13 +0500

    Implements hash verify

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -18,5 +19,9 @@ class AuthController extends Controller
     {
         $user = User::where('email', $request->email)
                     ->first();
+
+        if (!Hash::check($request->password, $user->password)) {
+            return getResponse(false, ['Invalid credentials']);
+        }
     }
 }
