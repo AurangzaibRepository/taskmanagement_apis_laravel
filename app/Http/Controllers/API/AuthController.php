0bc244b3914cc24c6406865daded61eee78f6efe
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 15:11:26 +0500

    Injects user model

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function __construct(
+        private User $user,
+    ) {
+    }
 }
