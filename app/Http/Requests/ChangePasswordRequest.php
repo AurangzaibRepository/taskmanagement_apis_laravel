d7399aa90734ba313fa7ef1861ec80dfb2ebc948
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-24 17:50:12 +0500

    Defines rule

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\PasswordRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ChangePasswordRequest extends FormRequest
@@ -9,6 +10,9 @@ class ChangePasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'email' => 'required|email|exists:users',
+            'password' => ['required', new PasswordRule($this->email)],
+            'new_password' => 'required',
         ];
     }
 }
