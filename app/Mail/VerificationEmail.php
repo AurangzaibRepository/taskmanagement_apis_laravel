f3ad677c20081d2957eaabbc6a71f1299aef4504
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 15:57:02 +0500

    Specifies view name

--- app/Mail/VerificationEmail.php
+++ app/Mail/VerificationEmail.php
@@ -27,7 +27,7 @@ class VerificationEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.verification-email',
         );
     }
 
