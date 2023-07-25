437ab3d13b79b6942e616ced0ab0648770b713ae
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 12:13:15 +0500

    Includes packages

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -3,7 +3,10 @@
 namespace App\Http\Requests;
 
 use App\Rules\PasswordRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class ChangePasswordRequest extends FormRequest
 {
