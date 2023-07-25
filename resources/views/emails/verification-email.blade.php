0861e774ded2187cf8fc4b9ef41aa4572721c5b8
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-25 17:59:09 +0500

    Write email template

---
+++ resources/views/emails/verification-email.blade.php
@@ -0,0 +1,9 @@
+Dear {{$user->first_name.' '.$user->last_name}},
+<br/><br/>
+You have requested to reset your password for Task Management account. Please refer to verification code below to verify your request.
+<br/>
+<b>{{$user->verification_code}}</b>
+<br/><br/>
+Best Regards,
+<br/>
+Support Task Management
