5dd4a3cc20a266549e26d7b3c26e83db72272ef3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-26 18:10:46 +0500

    Implements test method for login

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,8 +2,31 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    public function test_auth_login(): void
+    {
+        $payload = [
+            'email' => 'user38@laravel.com',
+            'password' => '12345678',
+        ];
+
+        $response = $this->postJson('/api/auth/login', $payload);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->hasAll([
+                    'data.first_name',
+                    'data.last_name',
+                    'data.email',
+                    'data.phone_number',
+                    'data.team_id',
+                    'data.department_id',
+                ])
+            );
+    }
 }
