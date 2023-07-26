4d6e8c9f03900e03032d41e46e4e30f926e45a7c
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-26 16:44:25 +0500

    Defines route for register

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('change-password', [AuthController::class, 'changePassword']);
     Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
