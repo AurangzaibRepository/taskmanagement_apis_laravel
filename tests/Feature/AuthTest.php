fe3cf210654a40cc72e80f19b1332cc91b697cae
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-26 18:42:29 +0500

    Implements test for change password

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -29,4 +29,21 @@ class AuthTest extends TestCase
                 ])
             );
     }
+
+    public function test_auth_change_password(): void
+    {
+        $payload = [
+            'email' => 'user38@laravel.com',
+            'password' => '12345678',
+            'new_password' => '123456',
+        ];
+
+        $response = $this->postJson('/api/auth/change-password', $payload);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) => $json->where('status', true)
+                ->has('message')
+            );
+    }
 }
