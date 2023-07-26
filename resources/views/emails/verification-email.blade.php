ae812d08820e0dd55252c351f1b6bc86707de662
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-26 15:21:26 +0500

    Adds verification code

--- resources/views/emails/verification-email.blade.php
+++ resources/views/emails/verification-email.blade.php
@@ -2,7 +2,7 @@ Dear {{$user->first_name.' '.$user->last_name}},
 <br/><br/>
 You have requested to reset your password for Task Management account. Please refer to link below to proceed.
 <br/>
-<b>{{route('resetPassword', ['code' => $user->verification_code])}}</b>
+<b>{{$user->verification_code}}</b>
 <br/><br/>
 Best Regards,
 <br/>
