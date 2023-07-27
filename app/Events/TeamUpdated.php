26ebd3ae2c8da94c20ea76a775992f1daf3ba704
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-27 20:22:32 +0500

    Includes team model

--- app/Events/TeamUpdated.php
+++ app/Events/TeamUpdated.php
@@ -2,11 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use App\Models\Team;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -17,8 +15,9 @@ class TeamUpdated
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public Team $team,
+    ) {
     }
 
     /**
