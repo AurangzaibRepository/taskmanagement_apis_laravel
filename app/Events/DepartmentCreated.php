1016f1e9f805cf33a3253589ae6b2cbb653fcdde
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-28 13:18:46 +0500

    Includes model

--- app/Events/DepartmentCreated.php
+++ app/Events/DepartmentCreated.php
@@ -2,11 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use App\Models\Department;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -17,8 +15,9 @@ class DepartmentCreated
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public Department $department,
+    ) {
     }
 
     /**
