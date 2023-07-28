b6dfb33c1c13afe68e50fbb4f86de913da88336a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-28 16:40:56 +0500

    Implements webhook call

--- app/Listeners/CreateTaskHandler.php
+++ app/Listeners/CreateTaskHandler.php
@@ -3,8 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\TaskCreated;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use Spatie\WebhookServer\WebhookCall;
 
 class CreateTaskHandler
 {
@@ -20,5 +19,17 @@ class CreateTaskHandler
      */
     public function handle(TaskCreated $event): void
     {
+        $data = [
+            'task' => $event->task,
+        ];
+        $data['task']['project'] = $event->task->project;
+        $data['task']['category'] = $event->task->category;
+        $data['task']['user'] = $event->task->user;
+
+        WebhookCall::create()
+            ->url(config('app.client_webhook_url'))
+            ->payload($$data)
+            ->useSecret(config('app.webhook_secret'))
+            ->dispatch();
     }
 }
