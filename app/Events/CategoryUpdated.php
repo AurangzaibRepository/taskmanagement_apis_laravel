18da7e01b7139dab135e58bc58e27b5dc20eaf70
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-28 12:59:35 +0500

    Incldues caetgory model

--- app/Events/CategoryUpdated.php
+++ app/Events/CategoryUpdated.php
@@ -2,11 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use App\Models\Category;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -17,8 +15,9 @@ class CategoryUpdated
     /**
      * Create a new event instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public Category $category,
+    ) {
     }
 
     /**
