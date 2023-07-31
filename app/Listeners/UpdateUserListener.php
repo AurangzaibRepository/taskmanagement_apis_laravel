4d2d81da159cc600c90910a07612f195f0fdf7bc
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-31 12:54:38 +0500

    Creates listener for user updated event

---
+++ app/Listeners/UpdateUserListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserUpdated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UpdateUserListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserUpdated $event): void
+    {
+    }
+}
