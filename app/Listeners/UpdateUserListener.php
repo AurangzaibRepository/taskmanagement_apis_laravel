386ef1b19913443b174abc4a7a9aa9726ac6abde
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-31 13:00:33 +0500

    Implements webhook

--- app/Listeners/UpdateUserListener.php
+++ app/Listeners/UpdateUserListener.php
@@ -3,8 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\UserUpdated;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use Spatie\WebhookServer\WebhookCall;
 
 class UpdateUserListener
 {
@@ -20,5 +19,17 @@ class UpdateUserListener
      */
     public function handle(UserUpdated $event): void
     {
+        $data = [
+            'user' => $event->user,
+        ];
+        $data['user']['team'] = $event->user->team;
+        $data['user']['department'] = $event->user->department;
+        $data['user']['tasks'] = $event->user->tasks;
+
+        WebhookCall::create()
+            ->url(config('app.client_webhook_url'))
+            ->payload($data)
+            ->useSecret(config('app.webhook_secret'))
+            ->dispatch();
     }
 }
